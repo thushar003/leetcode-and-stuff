dcfcc951351b84ca888fe0a8245882f9ed3ef57c
Thushar Joji <68509831+thushar003@users.noreply.github.com>
2025-04-29 18:38:20 -0400

    Time: 0 ms (100%), Space: 12.4 MB (50.64%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        digits = digits[::-1]
+        one = 1
+        i = 0
+        while one:
+            if i < len(digits):
+                if digits[i] == 9:
+                    digits[i] = 0
+                else:
+                    digits[i] += 1
+                    one = 0
+            else:
+                digits.append(1)
+                one = 0
+            i += 1
+        return digits[::-1]
