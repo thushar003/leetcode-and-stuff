0dd13cd78622a6c5b39379c4af6b0a4fbeab208d
Thushar Joji <68509831+thushar003@users.noreply.github.com>
2026-05-07 17:11:54 -0400

    Time: 0 ms (100%), Space: 12.5 MB (17.11%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -4,18 +4,11 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        digits = digits[::-1]
-        one = 1
-        i = 0
-        while one:
-            if i < len(digits):
-                if digits[i] == 9:
-                    digits[i] = 0
-                else:
-                    digits[i] += 1
-                    one = 0
-            else:
-                digits.append(1)
-                one = 0
-            i += 1
-        return digits[::-1]
+        if not digits:
+            return [1]
+
+        if digits[-1] < 9:
+            digits[-1] += 1
+            return digits
+        else:
+            return self.plusOne(digits[:-1]) + [0]
